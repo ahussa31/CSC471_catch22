0bd39677949111db586934c4fca5fd4813a3d285
aemen <aemen@dscout.com>
2016-03-15 02:31:02 -0500

    added icons and attempted code clean up

--- catch22v2/catch22/modelData.swift
+++ catch22v2/catch22/modelData.swift
@@ -9,25 +9,13 @@
 import UIKit
 
 class Main {
-//    var name:String
-//    var time:String
-//    var response:String
     var arrivalArr = [String]()
     var mode:String
     init(arrivalArr:[String], mode:String) {
-//        self.name = name
-//        self.time = time
-//        self.response = response
        self.arrivalArr = arrivalArr
        self.mode = mode
     }
-    
-
-
-    
 }
-//
-//var mainInstance = Main(name:"My Global Class",time:"", response:"", arrivalArr: [])
 
 var mainInstance = Main ( arrivalArr: [], mode: "")
 
